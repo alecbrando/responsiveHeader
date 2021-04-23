f2ea9f59e1dc13d113b0a9f1a8e91b276800095d
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-22 20:18:12 -0700

    put files in the pages folder and seperated with home in appjs

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  );
+}
+
+export default Home;
